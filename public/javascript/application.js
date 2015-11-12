8014bbd0c6739a182ab8c4394f1e80b70d1749a0
Hamza Sheikh <hsheikhm@gmail.com>
2015-11-12 11:01:05 +0000

    refactored application.js

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -7,22 +7,22 @@ $("#psm").prepend(thermostat.powerSaving);
 
 $(document).on("click", "#up-temp", function() {
   thermostat.upTemp();
-  $("#current-temp").replaceWith("<span id='current-temp'>" + thermostat.currentTemp + "</span>");
+  $("#current-temp").text(thermostat.currentTemp);
 });
 
 $(document).on("click", "#down-temp", function() {
   thermostat.downTemp();
-  $("#current-temp").replaceWith("<span id='current-temp'>" + thermostat.currentTemp + "</span>");
+  $("#current-temp").text(thermostat.currentTemp);
 });
 
 $(document).on("click", "#reset-temp", function() {
   thermostat.resetTemp();
-  $("#current-temp").replaceWith("<span id='current-temp'>" + thermostat.currentTemp + "</span>");
+  $("#current-temp").text(thermostat.currentTemp);
 });
 
 $(document).on("click", "#power-switch", function() {
   thermostat.powerSavingSwitch();
-  $("#psm").replaceWith("<span id='psm'>" + thermostat.powerSaving + "</span>");
+  $("#psm").text(thermostat.powerSaving);
 });
 
 });
